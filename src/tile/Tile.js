aadfdb906f42d6961c4000ef1abc9ca4a1a525a1
Tatiana Veratti <tatianaveratti@gmail.com>
2018-06-04 21:16:44 -0500

    initial build config files and app component

---
+++ src/tile/Tile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+
+import './Tile.scss';
+
+class Tile extends Component {
+  render() {
+    return <div className="tile" />;
+  }
+}
+
+export default Tile;
